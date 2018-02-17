152b779a8b3e06ada903125ec69ef77c2b83063e
Elena <lena.ciumeica@parkopedia.com>
2018-02-17 13:33:55 +0000

    Merge branch 'admin_page' of https://github.com/mihaineghin/titanium-javascript-hackaton into elena
    # Conflicts:
    #	app.js
    #	controllers/admin.js
    #	models/Lessons.js
    #	views/admin/lessons/lesson-form.pug

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
 const taskController = require('./controllers/task');
+const adminController = require('./controllers/admin');
 
 /**
  * API keys and Passport configuration.
